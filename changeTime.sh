b4536ae32e3f23a07c4b28bcaa41dde8c54a2be1
huyhoang8398 <huyhoang8398@gmail.com>
2018-12-24 23:14:40 +0700

    v1.1

--- changeTime.sh
+++ changeTime.sh
@@ -4,7 +4,10 @@ infoDPI=$(grep java cron.txt| awk '{print $(NF-2)}')
 sed -i -e 's/\r//g' outputTime.txt
 newCron=$(cat -A outputTime.txt)
 crontab -u pi -l | grep -v java > cron.txt
+
+#change your path to your java file that control the scaner 
 echo -e "${newCron}" "java -jar /home/pi/scannn/scan-lib.jar" "${infoDPI}" "> /dev/null" >> cron.txt
+
 #sed -i "s|$oldCron||g" fileTmp.txt
 #sed -i "s|$oldCron|$newCron|g" fileTmp.txt
 crontab -u pi cron.txt
